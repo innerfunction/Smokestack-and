c76102e6e4c9ec286574c9402f60f53b90aa4d12
Julian Goacher <julian.goacher@innerfunction.com>
2017-03-15 18:25:44 +0000

    Removed obsolete import.

--- Smokestack/src/main/java/com/innerfunction/smokestack/commands/CommandProtocol.java
+++ Smokestack/src/main/java/com/innerfunction/smokestack/commands/CommandProtocol.java
@@ -6,8 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static com.innerfunction.smokestack.commands.CommandScheduler.CommandItem;
-
 /**
  * A command protocol.
  * A command implementation that supports multiple different named commands, useful for
